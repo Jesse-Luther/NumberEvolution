7a7267769476a882ce75f6f83f1686aeeef2f2ee
Jesse-Luther <jesseluther@outlook.com>
2022-10-13 05:40:36 -0400

    Add UpdateEntityPosition function
    Also changed the Entity position storage from two variables(X and Y position) to an array that holds  [X,Y].

---
+++ Entity.cpp
@@ -0,0 +1,37 @@
+/*
+	Filename: Entity.cpp
+	Description: Contains functions to track information about entities
+	that will populate the grid environment.
+*/
+
+//includes
+#include "Entity.h"
+
+//defines
+
+Entity::Entity(int posX, int posY) {
+	std::cout << "Entity Created";
+}
+
+/*
+	Allows the entity to move in a single direction (up/down = X, left/right = Y)
+	by a specified amount. Notably, downards is positive X, while upwards is negative X. 
+
+	Parameters:
+		-postionChange: either 'x' or 'y' to represent which positional value is being 
+		changed
+		-amount: the amount the prior value is being changed by (can be positive or 
+		negative
+*/
+void Entity::UpdateEntityPosition(char positionChange, int amount) {
+	if (positionChange == 'x') {
+		entityPosition[0] += amount;
+	}
+	else if (positionChange == 'y') {
+		entityPosition[1] += amount;
+	}
+		//make the final else throw an exception instead of stating error
+	else {
+		std::cout << "An improper position type has been provided";
+	}
+}
--- Entity.h
+++ Entity.h
@@ -1,14 +1,22 @@
 #pragma once
+/*
+	Filename: Entity.h
+	Description: Contains functions to track information about entities
+	that will populate the grid environment.
+*/
 class Entity {
 
 public:
 
+	Entity(int posX, int posY);
 	void UpdateEntityPosition(char positionChange, int amount);
+	//int[] GetEntityPosition();
+	//void SetEntityPosition(int posX, int posY); used for placing directly on the board onces instantiated
+	
 
 private:
 
-	int entityXPosition;
-	int entityYPosition;
-
+	int entityPosition[2] = { 0,0 };
+	
 
 };
