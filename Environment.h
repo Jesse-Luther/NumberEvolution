3e033fe1af95ff5ecaddfafb7f91ca55349a8d8c
Jesse-Luther <jesseluther@outlook.com>
2022-10-18 16:04:34 -0400

    Reimplement environmentGrid as vector
    environmentGrid now functions as a single dimensional vector rather than a multidimensional array. This will allow the use of a single, mutable structure to be usable for each grid size, including potential grid sizes to be added in the future.

--- Environment.h
+++ Environment.h
@@ -8,19 +8,23 @@
 //includes
 
 
-//defines
-#define GRID_WIDTH 10
-#define GRID_HEIGHT 10
-
 class Environment {
 
 public:
 
 	Environment();
 
-	std::string environmentGrid[GRID_WIDTH][GRID_HEIGHT];
+	//different grid sizes
+	const int GRIDW_10X10 = 10;
+	const int GRIDH_10X10 = 10;
+
+	std::vector<std::string> environmentGrid;
 
 	void InitEnvironment(); 
 	void UpdateEnivronment(const int posX, const int posY, const std::string updateValue);
 	void PrintEnvironment();
+
+private:
+
+
 };
