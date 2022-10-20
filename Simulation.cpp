288e780b295fc8d20d117bbfca80c3916aab4cdf
Jesse-Luther <jesseluther@outlook.com>
2022-10-20 05:29:35 -0400

    Add simulation.cpp and simulation.h
    These will contain the Simulation class, which handles functions involving setting up and running different simulation test cases.

---
+++ Simulation.cpp
@@ -0,0 +1,8 @@
+/*
+	Filename: Simulation.cpp
+	Description: Handles processes that are involved in running the simulation
+	by controlling the entities, entity movements, and food generation.
+*/
+
+//includes
+#include "Simulation.h"
---
+++ Simulation.h
@@ -0,0 +1,6 @@
+#pragma once
+/*
+	Filename: Simulation.h
+	Description: Handles processes that are involved in running the simulation
+	by controlling the entities, entity movements, and food generation.
+*/
