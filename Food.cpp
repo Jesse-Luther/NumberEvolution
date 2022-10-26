7655b22ba2d54a8fb69f5d2e7da7a88bf950a9fe
Jesse-Luther <jesseluther@outlook.com>
2022-10-26 03:06:18 -0400

    Add files for Food class

---
+++ Food.cpp
@@ -0,0 +1,7 @@
+/*
+	Filename: Food.cpp
+	Description: Handles processes involving the spawning and removing of food 
+	from the grid environment.
+*/
+
+//includes
---
+++ Food.h
@@ -0,0 +1,8 @@
+#pragma once
+/*
+	Filename: Food.h
+	Description: Handles processes involving the spawning and removing of food 
+	from the grid environment.
+*/
+
+//includes
