0f4bf5998e3193ce07a69c8036921c7585e8d9c5
Jesse-Luther <jesseluther@outlook.com>
2022-10-30 04:44:25 -0400

    Add files for Trait Inheritance class

---
+++ TraitInheritance.cpp
@@ -0,0 +1,6 @@
+/*
+	Filename: TraitInheritance.cpp
+	Description: Contains functions to store information about the traits that will be passed 
+	from parent to offspring entities, as well as functions to make determine if and how traits 
+	will be passed.
+*/
---
+++ TraitInheritance.h
@@ -0,0 +1,7 @@
+#pragma once
+/*
+	Filename: TraitInheritance.h
+	Description: Contains functions to store information about the traits that will be passed
+	from parent to offspring entities, as well as functions to make determine if and how traits
+	will be passed.
+*/
