2b080a956bcf30c68c74959a75f5d723f590e1e1
Jesse-Luther <jesseluther@outlook.com>
2022-11-23 17:37:20 -0500

    Add initial implementation of TraitInheritance class

--- TraitInheritance.h
+++ TraitInheritance.h
@@ -2,6 +2,25 @@
 /*
 	Filename: TraitInheritance.h
 	Description: Contains functions to store information about the traits that will be passed
-	from parent to offspring entities, as well as functions to make determine if and how traits
+	from parent to offspring entities, as well as functions to determine if and how traits
 	will be passed.
 */
+
+struct EntityData {
+	string id = "0";
+
+	int foodEaten = 0;
+	int offSpringNum = 0;
+};
+
+class TraitInheritance {
+
+public:
+	//determines if the passed entity has eaten enough food. Returns 1 if it has, and 0 if it has not. Also, 
+	//stores data about how much food it consumed and 
+	int checkSurvival(Entity &entity);
+
+private:
+	std::vector<EntityData> generationalDataStorage;
+
+};
